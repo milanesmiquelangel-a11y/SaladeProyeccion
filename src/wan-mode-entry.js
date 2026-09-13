b4c96053de88bfcb8fc2fa6868eb911da255352b
milanesmiquelangel-a11y <milanes.miquelangel@gmail.com>
2026-09-13 12:06:48 +0500

    Enable administration profile

--- src/wan-mode-entry.js
+++ src/wan-mode-entry.js
@@ -1,4 +1,5 @@
 import './wan-mode-bootstrap.js';
 import './pixazo-free-prompt-bootstrap.js';
 import './final-generation-bootstrap.js';
+import './admin-bootstrap.js';
 await import('./monetized-entry.js');
