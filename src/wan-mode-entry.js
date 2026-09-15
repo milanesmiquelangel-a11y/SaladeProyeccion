8d4ea8e0bdf9456014350d5281f475ba8bd3d93b
milanesmiquelangel-a11y <milanes.miquelangel@gmail.com>
2026-09-15 23:33:21 +0500

    Load Pixazo speed optimization before generation

--- src/wan-mode-entry.js
+++ src/wan-mode-entry.js
@@ -1,5 +1,6 @@
 import './wan-mode-bootstrap.js';
 import './pixazo-free-prompt-bootstrap.js';
+import './pixazo-free-speed-patch.js';
 import './final-generation-bootstrap.js';
 import './generation-error-diagnostic.js';
 import './admin-bootstrap.js';
